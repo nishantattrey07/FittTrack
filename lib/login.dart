655dce345b5723db35b9f6a22e16a67ee85bf873
nishantattrey07 <nishantattrey07@gmail.com>
2024-04-13 17:22:36 +0530

    Add user signup functionality and update app routes and login

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 // // Uri.parse(''),
 
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -7,10 +8,11 @@ import 'app_routes.dart';
 import 'dart:convert';
 import 'Signup.dart';
 
-
 class login extends StatelessWidget {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final FocusNode _usernameFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
 
   Future<void> _login(
       String username, String password, BuildContext context) async {
@@ -30,96 +32,172 @@ class login extends StatelessWidget {
         Map<String, dynamic> responseBody = jsonDecode(response.body);
         String token = responseBody['token'];
 
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Login successful')),
-        );
+    
         Navigator.pushNamed(context, AppRoutes.third);
       } else {
         throw Exception('Failed to login');
       }
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(e.toString())),
-      );
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   SnackBar(content: Text(e.toString())),
+      // );
+      print(e);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('FittTrack'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.blue,
+          canvasColor: const Color.fromARGB(255, 48, 47, 47),
+          fontFamily: 'Metropolis',
+          brightness: Brightness.dark,
+        ),
+        home: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 32, 32, 32),
+          body: Stack(
+            children: [
+              Positioned(
+                top: MediaQuery.of(context).size.height *
+                    0.15, // Adjust this value as needed
+                left: MediaQuery.of(context).size.width *
+                    0.05, // Adjust this value as needed
+                child: Text(
+                  'Log In',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: FractionallySizedBox(
+                  heightFactor: 0.75,
+                  child: Card(
+                    color: Colors.white,
+                    // ...
+                    child: Padding(
+                      padding: EdgeInsets.all(35.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                focusNode: _usernameFocusNode,
+                                controller: _usernameController,
+                                style: TextStyle(color: Colors.black),
+                                cursorColor: Colors.blue,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue),
+                                  ),
+                                  labelText: 'Username',
+                                  labelStyle: TextStyle(
+                                    color: _usernameFocusNode.hasFocus
+                                        ? Colors.blue
+                                        : Color.fromARGB(255, 107, 105, 105),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 1.0),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                focusNode: _passwordFocusNode,
+                                controller: _passwordController,
+                                cursorColor: Colors.blue,
+                                style: TextStyle(color: Colors.black),
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Password',
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue),
+                                  ),
+                                  labelStyle: TextStyle(
+                                    color: _passwordFocusNode.hasFocus
+                                        ? Colors.blue
+                                        : Color.fromARGB(255, 107, 105, 105),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 1.0),
+                            ElevatedButton(
+                              onPressed: () {
+                                _login(
+                                  _usernameController.text,
+                                  _passwordController.text,
+                                  context,
+                                );
+                              },
+                              child: Text('Sign In',
+                                  style: TextStyle(color: Colors.white)),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    Colors.blue, // background color
+                                foregroundColor: Colors.white, // text color
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.circular(30), // curved edges
+                                ),
+                                minimumSize: Size(double.infinity,
+                                    50), // make the button long
+                              ),
+                            ),
+                            SizedBox(height: 90),
+                            Divider(
+                              color: Colors.grey,
+                              height: 20,
+                              thickness: 1,
+                              indent: 20,
+                              endIndent: 20,
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'New here?',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.pushNamed(
+                                        context, AppRoutes.second);
+                                  },
+                                  child: Text(
+                                    ' Sign Up',
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+// ...
+// ...
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-              ),
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                _login(
-                  _usernameController.text,
-                  _passwordController.text,
-                  context,
-                );
-              },
-            ),
-            SizedBox(height: 16.0),
-            TextButton(
-              child: Text(
-                'Forgot password?',
-                style: TextStyle(
-                  color: Colors.blue,
-                ),
-              ),
-              onPressed: () {
-                
-              },
-            ),
-            SizedBox(height: 16.0),
-            TextButton(
-              child: Text(
-                'Sign Up',
-                style: TextStyle(
-                  color: Colors.blue,
-                ),
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, AppRoutes.second);
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
